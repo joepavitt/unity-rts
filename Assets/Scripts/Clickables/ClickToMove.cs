9f63e300158449299ec9ffa6d4764c87f8ffaad2
Joe Pavitt <joepavit@uk.ibm.com>
2017-07-25 21:39:53 +0100

    Initial Setup of RTS Environment - Setup RTS Camera - Right-click to move unit - NavMesh to test navigation - Add obstacle to NavMesh testing - SelectionManager to store current selection (left-click)

---
+++ Assets/Scripts/Clickables/ClickToMove.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickToMove : MonoBehaviour {
+
+	private NavMeshAgent navMeshAgent;
+
+	// Use this for initialization
+	void Awake () {
+		navMeshAgent = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+
+		if (Input.GetButtonDown ("Fire2")) 
+		{
+			if (Physics.Raycast (ray, out hit, 100))
+			{
+				navMeshAgent.destination = hit.point;
+				navMeshAgent.Resume();
+			}
+		}
+	}
+}
